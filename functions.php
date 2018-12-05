d9116f57f08498f057de9d7b006f5c51ad887db4
Hassan AIT TAMAA <h.aittamaa@gmail.com>
2018-12-05 17:47:24 +0100

    Replace get_template_directory by get_theme_file_path

--- functions.php
+++ functions.php
@@ -5,49 +5,49 @@
  */			
 	
 // Theme support options
-require_once(get_template_directory().'/functions/theme-support.php'); 
+require_once(get_theme_file_path('/functions/theme-support.php')); 
 
 // WP Head and other cleanup functions
-require_once(get_template_directory().'/functions/cleanup.php'); 
+require_once(get_theme_file_path('/functions/cleanup.php')); 
 
 // Register scripts and stylesheets
-require_once(get_template_directory().'/functions/enqueue-scripts.php'); 
+require_once(get_theme_file_path('/functions/enqueue-scripts.php')); 
 
 // Register custom menus and menu walkers
-require_once(get_template_directory().'/functions/menu.php'); 
+require_once(get_theme_file_path('/functions/menu.php')); 
 
 // Register sidebars/widget areas
-require_once(get_template_directory().'/functions/sidebar.php'); 
+require_once(get_theme_file_path('/functions/sidebar.php')); 
 
 // Makes WordPress comments suck less
-require_once(get_template_directory().'/functions/comments.php'); 
+require_once(get_theme_file_path('/functions/comments.php')); 
 
 // Replace 'older/newer' post links with numbered navigation
-require_once(get_template_directory().'/functions/page-navi.php'); 
+require_once(get_theme_file_path('/functions/page-navi.php')); 
 
 // Adds support for multiple languages
-require_once(get_template_directory().'/functions/translation/translation.php'); 
+require_once(get_theme_file_path('/functions/translation/translation.php')); 
 
 // Adds site styles to the WordPress editor
-require_once(get_template_directory().'/functions/editor-styles.php'); 
+require_once(get_theme_file_path('/functions/editor-styles.php')); 
 
-// Remove Emoji Support
-require_once(get_template_directory().'/functions/disable-emoji.php'); 
+// Remove 4.2 Emoji Support
+require_once(get_theme_file_path('/functions/disable-emoji.php')); 
 
 // Related post function - no need to rely on plugins
-// require_once(get_template_directory().'/functions/related-posts.php'); 
+// require_once(get_theme_file_path('/functions/related-posts.php')); 
 
 // Use this as a template for custom post types
-// require_once(get_template_directory().'/functions/custom-post-type.php');
+// require_once(get_theme_file_path('/functions/custom-post-type.php');
 
 // Customize the WordPress login menu
-require_once(get_template_directory().'/functions/login.php'); 
+require_once(get_theme_file_path('/functions/login.php')); 
 
 // Customize the WordPress admin
-require_once(get_template_directory().'/functions/admin.php'); 
+require_once(get_theme_file_path('/functions/admin.php')); 
 
 // Load Caldera custom filters  - replace bootstrap by foundation
-require_once(get_template_directory().'/functions/caldera-filters.php'); 
+//require_once(get_theme_file_path('/functions/caldera-filters.php')); 
 
 // Enable which template file feature
-// require_once(get_template_directory().'/functions/which-template.php');
+require_once(get_theme_file_path('/functions/which-template.php'));
